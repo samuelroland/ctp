52dee08b29e9b5f1987f8d03fa365c13d2b7be69
Samuel Roland <samuelroland@noreply.codeberg.org>
2024-05-20 03:35:42 +0200

    Disable .cpp parsing in CppConverter

--- src/CppConverter.java
+++ src/CppConverter.java
@@ -1,12 +1,10 @@
 class CppConvertor extends Converter {
     private static final String[] args = new String[] {
             "hpp2plantuml",
-            "-i", Converter.FOLDER_INSERT + "/*.h",
             "-i", Converter.FOLDER_INSERT + "/**/*.h",
-            "-i", Converter.FOLDER_INSERT + "/*.hpp",
             "-i", Converter.FOLDER_INSERT + "/**/*.hpp",
-            "-i", Converter.FOLDER_INSERT + "/*.cpp",
-            "-i", Converter.FOLDER_INSERT + "/**/*.cpp",
+            // "-i", Converter.FOLDER_INSERT + "/**/*.cpp", //Do not parse .cpp as it's
+            // generally useless and not always working... see readme of hpp2plantuml
             "-o", Converter.FILE_INSERT };
 
     CppConvertor() {
